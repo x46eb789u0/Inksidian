a0f8134ea4b09bb44ff3b9cee6d31f8f7318cc4d
x46eb789u0 <jackmilo426@gmail.com>
2025-10-02 00:14:39 -0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -64,14 +64,14 @@ local gethui = gethui or function()
     end) or PlayerGui
 end
 
---[[ if getrenv and setreadonly then
+if getrenv and setreadonly then
     pcall(function()
         local env = getrenv()
         setreadonly(env, false)
         env.script = nil
         setreadonly(env, true)
     end)
-end ]]--
+end
 
 local gc_protect = function(tbl)
     pcall(function()
